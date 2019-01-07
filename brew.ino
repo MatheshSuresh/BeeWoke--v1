4c309e0f29263a92642047ffcdec100d41e83324
Mathesh Suresh <33441626+MatheshSuresh@users.noreply.github.com>
2019-01-07 14:10:23 +0530

    Add files via upload

--- brew.ino
+++ brew.ino
@@ -1,11 +1,4 @@
-/*
-Liquid flow rate sensor -DIYhacking.com Arvind Sanjeev
 
-Measure the liquid/water flow rate using this code. 
-Connect Vcc and Gnd of sensor to arduino, and the 
-signal line to arduino digital pin 2.
- 
- */
 
 int data =1;
 int i =0;
@@ -15,8 +8,6 @@ byte statusLed    = 5;
 byte sensorInterrupt = 0;
 byte sensorPin       = 12;
 
-// The hall-effect flow sensor outputs approximately 4.5 pulses per second per
-// litre/minute of flow.
 float calibrationFactor = 4.5;
 
 volatile byte pulseCount;  
@@ -29,23 +20,22 @@ unsigned long oldTime;
 
 int valve1 = 5;
 int valve2 = 6;
-
+int coil = 7;
 int valve[2] = {valve1,valve2};
 
+Servo servo;
+
 void reset();
+void pour();
 
 void setup()
 {
-  
-  // Initialize a serial connection for reporting values to the host
   Serial.begin(9600);
-   
-  // Set up the status LED line as an output
-  pinMode(statusLed, OUTPUT);
-  digitalWrite(statusLed, HIGH);  // We have an active-low LED attached
-  
+  pinMode(coil,OUTPUT);  
   pinMode(sensorPin, INPUT);
   digitalWrite(sensorPin, HIGH);
+  servo.attach(8);  
+  servo.write(angle);
 
   pulseCount        = 0;
   flowRate          = 0.0;
@@ -53,84 +43,128 @@ void setup()
   totalMilliLitres  = 0;
   oldTime           = 0;
 
-  // The Hall-effect sensor is connected to pin 2 which uses interrupt 0.
-  // Configured to trigger on a FALLING state change (transition from HIGH
-  // state to LOW state)
   attachInterrupt(sensorInterrupt, pulseCounter, FALLING);
+  Serial.println("start");
 }
 
-/**
- * Main program loop
- */
 void loop()
 {
+   if(Serial.available()>0)
+   {     
+     { char data1= Serial.read();
+      switch(data1)
+      {
+        case 'r':{
+          digitalWrite(coil,HIGH);
+          
+          break;
+          }
+        case '4':{
+          pour();
+          for(angle = 10; angle < 60; angle++)  
+          {                                  
+            servo.write(angle);               
+            delay(500);                   
+          } 
+          pour();
+          for(angle = 60; angle < 120; angle++)  
+          {                                  
+            servo.write(angle);               
+            delay(500);                   
+          } 
+          pour();
+          for(angle = 120; angle < 180; angle++)  
+          {                                  
+            servo.write(angle);               
+            delay(500);                   
+          } 
+          pour();
+          for(angle = 180; angle < 10; angle--)  
+          {                                  
+            servo.write(angle);               
+            delay(500);                   
+          } 
+          break;
+          }
+        case '3':{
+          pour();
+          for(angle = 10; angle < 60; angle++)  
+          {                                  
+            servo.write(angle);               
+            delay(500);                   
+          } 
+          pour();
+          for(angle = 60; angle < 120; angle++)  
+          {                                  
+            servo.write(angle);               
+            delay(500);                   
+          } 
+          pour();
+          for(angle = 120; angle < 10; angle--)  
+          {                                  
+            servo.write(angle);               
+            delay(500);                   
+          } 
+          break;          
+          }
+        case '2':{
+          pour();
+          for(angle = 10; angle < 60; angle++)  
+          {                                  
+            servo.write(angle);               
+            delay(500);                   
+          } 
+          pour();
+          for(angle = 60; angle < 10; angle--)  
+          {                                  
+            servo.write(angle);               
+            delay(500);                   
+          } 
+          break;
+          }
+          case '1':{
+              pour();
+              delay(500);
+              break;
+            }
+        default : break;
+            }
+            Serial.println(data1);
+   }
    if(data==1){
-    if((millis() - oldTime) > 1000)    // Only process counters once per second
-  {
-    digitalWrite(valve[i],HIGH); 
-    // Disable the interrupt while calculating flow rate and sending the value to
-    // the host
-    detachInterrupt(sensorInterrupt);
-        
-    // Because this loop may not complete in exactly 1 second intervals we calculate
-    // the number of milliseconds that have passed since the last execution and use
-    // that to scale the output. We also apply the calibrationFactor to scale the output
-    // based on the number of pulses per second per units of measure (litres/minute in
-    // this case) coming from the sensor.
-    flowRate = ((1000.0 / (millis() - oldTime)) * pulseCount) / calibrationFactor;
-    
-    // Note the time this processing pass was executed. Note that because we've
-    // disabled interrupts the millis() function won't actually be incrementing right
-    // at this point, but it will still return the value it was set to just before
-    // interrupts went away.
-    oldTime = millis();
-    
-    // Divide the flow rate in litres/minute by 60 to determine how many litres have
-    // passed through the sensor in this 1 second interval, then multiply by 1000 to
-    // convert to millilitres.
-    flowMilliLitres = (flowRate / 60) * 1000;
-    
-    // Add the millilitres passed in this second to the cumulative total
-    totalMilliLitres += flowMilliLitres;
-      
-    unsigned int frac;
-    
-    // Print the flow rate for this second in litres / minute
-    Serial.print("Flow rate: ");
-    Serial.print(int(flowRate));  // Print the integer part of the variable
-    Serial.print("L/min");
-    Serial.print("\t");       // Print tab space
-
-    // Print the cumulative total of litres flowed since starting
-    Serial.print("Output Liquid Quantity: ");        
-    Serial.print(totalMilliLitres);
-    Serial.println("mL"); 
-    Serial.print("\t");       // Print tab space
-  Serial.print(totalMilliLitres/1000);
-  Serial.print("L");
-    
-
-    // Reset the pulse counter so we can start incrementing again
-    pulseCount = 0;
-    
-    // Enable the interrupt again now that we've finished sending output
-    attachInterrupt(sensorInterrupt, pulseCounter, FALLING);
-  } 
-  if(i==0){
-  if(totalMilliLitres>20)
-    digitalWrite(valve[i],LOW);
-    ++i;
-    digitalWrite(valve[i],HIGH);
-      }
-    }
-  if(i==1){
-    if(totalMilliLitres>120)
-    digitalWrite(valve[i],LOW);
-    reset();
-      } 
-    }
+      pour(); 
+   }
+  }
+ }
    
-
+void pour(){
+      if((millis() - oldTime) > 1000){
+          digitalWrite(valve[i],HIGH); 
+      
+          detachInterrupt(sensorInterrupt);
+              
+          flowRate = ((1000.0 / (millis() - oldTime)) * pulseCount) / calibrationFactor;
+      
+          oldTime = millis();
+          flowMilliLitres = (flowRate / 60) * 1000;
+          totalMilliLitres += flowMilliLitres;
+            
+          unsigned int frac;
+          pulseCount = 0;
+          attachInterrupt(sensorInterrupt, pulseCounter, FALLING);
+           if(i==0){
+            if(totalMilliLitres>20)
+              digitalWrite(valve[i],LOW);
+              ++i;
+              digitalWrite(valve[i],HIGH);
+                }
+            if(i==1){
+              if(totalMilliLitres>120)
+              digitalWrite(valve[i],LOW);
+              reset();
+            }
+          }
+        }
 
 void reset(){
   pulseCount        = 0;
